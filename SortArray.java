5ff22d5f195306c4951ebad197e86a11692795e6
rajavigneshwarank <rajavigneshwarank@gmail.com>
2025-10-15 13:02:20 +0530

    Updated sorting logic in SortArray.java for better performance

--- SortArray.java
+++ SortArray.java
@@ -14,8 +14,19 @@ public class SortArray{
             arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr);
-
+        
+        
+         int len = arr.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                   
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
         System.out.println("Result ");
         for (int num : arr) {
             System.out.print(num + " ");
